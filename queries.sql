daacfa90eade197638153434713c6ec7f3bfb23c
giannishon27-max <giannishon27@hotmail.gr>
2026-03-09 12:46:47 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -9,3 +9,22 @@ FROM netflix_titles
 GROUP BY country
 ORDER BY total_titles DESC
 LIMIT 10;
+
+-- 3. Titles per release year
+SELECT release_year, COUNT(*) AS total_titles
+FROM netflix_titles
+GROUP BY release_year
+ORDER BY release_year DESC;
+
+-- 4. Most common ratings
+SELECT rating, COUNT(*) AS total
+FROM netflix_titles
+GROUP BY rating
+ORDER BY total DESC;
+
+-- 5. Top 10 genres
+SELECT listed_in, COUNT(*) AS total
+FROM netflix_titles
+GROUP BY listed_in
+ORDER BY total DESC
+LIMIT 10;
